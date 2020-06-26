a17115d084e840a5dac673fa65ce83d2b53ddd03
karhites <hitesh.kar.123@gmail.com>
2020-06-26 14:12:58 +0200

    use build version for the web-page

---
+++ precache-manifest.3790fd1dec329b067f3c581b7fd2c6db.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "172f8cdffb483e01c262",
+    "url": "/css/app.16c036e0.css"
+  },
+  {
+    "revision": "cdacbf2be9c79b410b4e",
+    "url": "/css/chunk-2ea7f466.1387b33c.css"
+  },
+  {
+    "revision": "16320ee484e63c620d16",
+    "url": "/css/chunk-4c999562.1387b33c.css"
+  },
+  {
+    "revision": "275dea7312865c0b7a7d",
+    "url": "/css/chunk-4ee38512.1387b33c.css"
+  },
+  {
+    "revision": "b07cbc2f70b9b450db03",
+    "url": "/css/chunk-507f0b77.fd33e6e6.css"
+  },
+  {
+    "revision": "6e9bfae0bf79233ffa9d",
+    "url": "/css/chunk-51a3fe17.1387b33c.css"
+  },
+  {
+    "revision": "ca34d86c21f838b19512",
+    "url": "/css/chunk-6b3e42d8.1387b33c.css"
+  },
+  {
+    "revision": "3e00c72ef5462bfaae61",
+    "url": "/css/chunk-835c1dc4.1387b33c.css"
+  },
+  {
+    "revision": "a5946068436ee7d41121",
+    "url": "/css/chunk-c8655bda.1387b33c.css"
+  },
+  {
+    "revision": "ca141ae23fcab630d119",
+    "url": "/css/chunk-vendors.141fb40b.css"
+  },
+  {
+    "revision": "0a841868c8f49f911fc1d2c08f593fea",
+    "url": "/img/esa/Baltic_blooms.jpg"
+  },
+  {
+    "revision": "051fc4a5187f1f944dbd33d6a9f4e29b",
+    "url": "/img/esa/Baltic_blooms.webp"
+  },
+  {
+    "revision": "57b015c00643aa770f893bf74349dfb2",
+    "url": "/img/esa/Japanese_archipelago.jpg"
+  },
+  {
+    "revision": "67e4e1c06d955f4f5d868b141222dc14",
+    "url": "/img/esa/Japanese_archipelago.webp"
+  },
+  {
+    "revision": "04ae364abd5e5a62368ae3e66ce45f99",
+    "url": "/img/esa/Korean_Peninsula.jpg"
+  },
+  {
+    "revision": "91b06e594e5e0daecdc29680e9159d21",
+    "url": "/img/esa/Korean_Peninsula.webp"
+  },
+  {
+    "revision": "b4fb399536789a1156050647d548785a",
+    "url": "/img/esa/Lake_George_Uganda.jpg"
+  },
+  {
+    "revision": "bd613f3d7688b6cfe24365b4483aee05",
+    "url": "/img/esa/Lake_George_Uganda.webp"
+  },
+  {
+    "revision": "88096c4466c9483779f046e95f279b63",
+    "url": "/img/esa/Reykjavik_Iceland.jpg"
+  },
+  {
+    "revision": "e4adbbc45d1fc10ca9f4e3c2f3cf9e46",
+    "url": "/img/esa/Reykjavik_Iceland.webp"
+  },
+  {
+    "revision": "110859cc14e2de6608db3f41c0ae7c00",
+    "url": "/img/esa/The_Lena_River.jpg"
+  },
+  {
+    "revision": "9c6e9ff1e09e2bef66d143f3e83b17f7",
+    "url": "/img/esa/The_Lena_River.webp"
+  },
+  {
+    "revision": "afc8c0b460dc20e0f7d4ad540a19e8bf",
+    "url": "/img/esa/Tromsoe_Norway.jpg"
+  },
+  {
+    "revision": "697a53fd3c65931f61f95bcca5886da1",
+    "url": "/img/esa/Tromsoe_Norway.webp"
+  },
+  {
+    "revision": "824c7dfb61db79368ca2deee0df8dfb5",
+    "url": "/img/esa/Zambezi_Delta_diversity.jpg"
+  },
+  {
+    "revision": "04cd6c6e0316555a6bb88da8cc798f85",
+    "url": "/img/esa/Zambezi_Delta_diversity.webp"
+  },
+  {
+    "revision": "699ac4a6cf6bb0d2e5da10eb99236afb",
+    "url": "/img/poster_Earth_From_Space.jpg"
+  },
+  {
+    "revision": "f7cead0f07f796b75e869eb30afb6914",
+    "url": "/img/starmesh_logo_dec_2019.png"
+  },
+  {
+    "revision": "a72a7cb60304889f87de6f7721e5b66b",
+    "url": "/img/video/Earth_From_Space.mp4"
+  },
+  {
+    "revision": "02331237eecc23031a0d2e6d75b0a9f0",
+    "url": "/img/video/Earth_From_Space.webm"
+  },
+  {
+    "revision": "27531adcd89c4b45a30c3abe955a458c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "172f8cdffb483e01c262",
+    "url": "/js/app.1ef6046a.js"
+  },
+  {
+    "revision": "bd70133d1bf4b97f43f3",
+    "url": "/js/chunk-2d0c4a9e.e95c59a0.js"
+  },
+  {
+    "revision": "cdacbf2be9c79b410b4e",
+    "url": "/js/chunk-2ea7f466.d20e61e9.js"
+  },
+  {
+    "revision": "16320ee484e63c620d16",
+    "url": "/js/chunk-4c999562.b4db5921.js"
+  },
+  {
+    "revision": "275dea7312865c0b7a7d",
+    "url": "/js/chunk-4ee38512.2c3405da.js"
+  },
+  {
+    "revision": "b07cbc2f70b9b450db03",
+    "url": "/js/chunk-507f0b77.dea8b96e.js"
+  },
+  {
+    "revision": "6e9bfae0bf79233ffa9d",
+    "url": "/js/chunk-51a3fe17.c6c1d79d.js"
+  },
+  {
+    "revision": "ca34d86c21f838b19512",
+    "url": "/js/chunk-6b3e42d8.2b442215.js"
+  },
+  {
+    "revision": "3e00c72ef5462bfaae61",
+    "url": "/js/chunk-835c1dc4.ac6bf7ab.js"
+  },
+  {
+    "revision": "a5946068436ee7d41121",
+    "url": "/js/chunk-c8655bda.79099d83.js"
+  },
+  {
+    "revision": "ca141ae23fcab630d119",
+    "url": "/js/chunk-vendors.f20fe855.js"
+  },
+  {
+    "revision": "93e5790e84e77da38657ea0d6516f714",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "02a6a4136d63399eef8869827d18aa07",
+    "url": "/robots.txt"
+  }
+]);
